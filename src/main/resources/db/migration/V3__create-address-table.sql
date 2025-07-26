896b4019e5272d30ce48446f73063252d9fa86c5
Kauan Santos <kauansantosdepontes@gmail.com>
2025-07-26 19:27:30 -0300

    feat: created migrations and repositories

---
+++ src/main/resources/db/migration/V3__create-address-table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE address
+(
+    id       UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    city     VARCHAR(100) NOT NULL,
+    uf       VARCHAR(100) NOT NULL,
+    event_id UUID         NOT NULL,
+    FOREIGN KEY (event_id) REFERENCES event (id) ON DELETE CASCADE
+);
